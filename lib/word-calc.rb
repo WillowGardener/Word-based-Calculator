065ea5e356f1bd388d6e1fab5495eb380f875d70
Austin Steward & Griffin Wimminger <student@epicodus.com>
2014-07-23 10:06:37 -0700

    Add, Subtract, Multiply words :moyai:

--- lib/word-calc.rb
+++ lib/word-calc.rb
@@ -1,10 +1,12 @@
 def word_calc(equation)
-  operator = equation.scan(/(plus)|(minus)/).join
+  operator = equation.scan(/(plus)|(minus)|(times)/).join
   numbers = equation.scan(/[0-9]/)
   if operator == "plus"
     solution = numbers[0].to_i + numbers[1].to_i
   elsif operator == "minus"
     solution = numbers[0].to_i - numbers[1].to_i
+  elsif operator == "times"
+    solution = numbers[0].to_i * numbers[1].to_i
   end
 
   solution
--- spec/word-calc_spec.rb
+++ spec/word-calc_spec.rb
@@ -9,4 +9,8 @@ describe("word_calc") do
   it("Subtracts one single digit number from another") do
     expect(word_calc("What is 5 minus 2?")).to(eq(3))
   end
+
+  it("Multiplies one single digit number to another") do
+    expect(word_calc("What is 9 times 8?")).to(eq(72))
+  end
 end
