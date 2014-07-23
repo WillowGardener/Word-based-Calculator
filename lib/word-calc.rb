52c7a23d051fbb9419ac1678abac38e3b9d3f06e
Austin Steward & Griffin Wimminger <student@epicodus.com>
2014-07-23 11:59:05 -0700

    refactor

--- lib/word-calc.rb
+++ lib/word-calc.rb
@@ -1,18 +1,35 @@
-def word_calc(equation)
-  operator = equation.scan(/(plus)|(minus)|(times)|(divided)|(power)/).join
-  numbers = equation.scan(/[0-9]{1,}/)
-  if operator == "plus"
-    solution = numbers[0].to_i + numbers[1].to_i
-  elsif operator == "minus"
-    solution = numbers[0].to_i - numbers[1].to_i
-  elsif operator == "times"
-    solution = numbers[0].to_i * numbers[1].to_i
-  elsif operator == "divided"
-    solution = numbers[0].to_i / numbers[1].to_i
-  elsif operator == "power"
-    solution = numbers[0].to_i ** numbers[1].to_i
-  end
-
-  solution
+def word_calc(num1, num2, operator)
+    if operator == "plus"
+      operators = ['+']
+      solution =   operators.map {|o| num1.to_i.public_send o,num2.to_i }
+    elsif operator == "minus"
+      operators = ['-']
+      solution =   operators.map {|o| num1.to_i.public_send o,num2.to_i }
+    elsif operator == "times"
+      operators = ['*']
+      solution =   operators.map {|o| num1.to_i.public_send o,num2.to_i }
+    elsif operator == "divided"
+      operators = ['/']
+      solution =   operators.map {|o| num1.to_i.public_send o,num2.to_i }
+    elsif operator == "power"
+      operators = ['**']
+      solution =   operators.map {|o| num1.to_i.public_send o,num2.to_i }
+    end
+  solution.join.to_i
 
 end
+
+def number_reader(equation)
+  numbers = equation.scan(/[0-9]{1,}/)
+  operator = equation.scan(/(plus|minus|times|divided|power)/)
+  total = 0;
+  operator.each do |i|
+      total += word_calc(numbers[0], numbers[1], i.join)
+    numbers.delete(numbers[1])
+    numbers.delete(numbers[0])
+  end
+  total
+end
+
+
+
--- spec/word-calc_spec.rb
+++ spec/word-calc_spec.rb
@@ -3,24 +3,30 @@ require("word-calc")
 
 describe("word_calc") do
   it("Adds two single digit numbers together") do
-    expect(word_calc("What is 2 plus 3?")).to(eq(5))
+    expect(word_calc("2", "3", "plus")).to(eq(5))
   end
 
   it("Subtracts one single digit number from another") do
-    expect(word_calc("What is 5 minus 2?")).to(eq(3))
+    expect(word_calc("5", "2", "minus")).to(eq(3))
   end
 
   it("Multiplies one single digit number to another") do
-    expect(word_calc("What is 9 times 8?")).to(eq(72))
+    expect(word_calc("9", "8", "times")).to(eq(72))
   end
   it("Divides one single digit number by another") do
-    expect(word_calc("What is 8 divided by 4?")).to(eq(2))
+    expect(word_calc("8", "4", "divided")).to(eq(2))
   end
 
   it("Adds one double digit number by another") do
-    expect(word_calc("What is 20 divided by 10?")).to(eq(2))
+    expect(word_calc("20", "10", "divided")).to(eq(2))
   end
   it("takes one number to the power of another") do
-    expect(word_calc("What is 20 to the 3rd power?")).to(eq(8000))
+    expect(word_calc("20", "3", "power")).to(eq(8000))
+  end
+end
+
+describe("number_reader") do
+  it("Returns the solution of two numbers") do
+    expect(number_reader("What is 20 times 10?")).to(eq(200))
   end
 end
